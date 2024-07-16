13ab971d1fe86b812102af7969f7ae5efdb2e3d8
Zaid Kotbi <zkotbi@e3r8p18.1337.ma>
2024-07-16 16:04:57 +0100

    lfahama

---
+++ src/game.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   game.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkotbi <zkotbi@1337.ma>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 15:45:05 by zkotbi            #+#    #+#             */
+/*   Updated: 2024/07/16 15:53:49 by zkotbi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MLX42/MLX42.h"
+#include "cub3D.h"
+
+void	color_floor_ceiling(t_data *data)
+{
+	const uint32_t	half = data->image->height / 2;
+	uint32_t		i;
+	uint32_t		j;
+
+	i = 0;
+	while (i < data->image->height)
+	{
+		j = 0;
+		while (j < data->image->width)
+		{
+			if (i > half)
+				mlx_put_pixel(data->image, j, i,
+					*(uint32_t *)&data->param->floor_color);
+			else
+				mlx_put_pixel(data->image, j, i,
+					*(uint32_t *)&data->param->celling_color);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	game(t_data *data, int width)
+{
+	const t_vec2f	pos = data->param->map_data->pos;
+	t_vec2f			point;
+	t_draw			draw;
+	double			delta;
+	double			angle;
+	int				x;
+
+	color_floor_ceiling(data);
+	delta = FOV / (WIDTH - 1);
+	angle = data->player_angle - FOV / 2;
+	x = 0;
+	while (x < width)
+	{
+		angle += delta;
+		point = get_wall_postion(data->param->map_data, pos, angle);
+		// DDALine(data, pixels(tmp), pixels(point)); // TODO : make better algo
+		draw.distance = vec_distance(pos, point);
+		draw.img = get_img_direc(point, data);
+		draw.x_pos = get_tex_x(point, angle, draw.img);
+		ver_line(data, x++, &draw);
+	}
+}
